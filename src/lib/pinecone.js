f5d44dfbdac09c7b84946a0077bd1e7c60464794
ehuntober <ehuntercrypto@gmail.com>
2024-05-05 22:45:31 +0100

    modified the project structure

--- src/lib/pinecone.js
+++
@@ -1,8 +0,0 @@
-import * as PineconeClient from '@pinecone-database/pinecone-client';
-
-const pinecone = new PineconeClient.PineconeClient({
-  environment: process.env.PINECONE_ENVIRONMENT, 
-  apiKey: process.env.PINECONE_API_KEY, 
-});
-
-export default pinecone;
